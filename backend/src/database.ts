4232db1628f3f68be6e14a929050434253cf7f59
Sai Velpula <achyuv@80a99748b585.ant.amazon.com>
2025-04-18 20:56:58 -0400

    updated database file to initiate startup queires

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 import sqlite3 from 'sqlite3';
-import {open} from 'sqlite';
+import { open } from 'sqlite';
 
 const initDatabase = async () => {
   const db = await open({
@@ -7,102 +7,114 @@ const initDatabase = async () => {
     driver: sqlite3.Database,
   });
 
-  // Create the category table
-  // await db.exec(`
-  //   CREATE TABLE IF NOT EXISTS category (
-  //     id INTEGER PRIMARY KEY AUTOINCREMENT,
-  //     name TEXT NOT NULL,
-  //     target INTEGER 
-  //   )
-  // `);
+  const deleteTables = false
+  const createTables = false
 
-  // // Create the task table
-  // await db.exec(`
-  //   CREATE TABLE IF NOT EXISTS task (
-  //     id INTEGER PRIMARY KEY AUTOINCREMENT,
-  //     title TEXT NOT NULL,
-  //     description TEXT,
-  //     start_time TEXT,
-  //     end_time TEXT,
-  //     weight INTEGER,
-  //     completed BOOLEAN NOT NULL DEFAULT 0,
-  //     category_id INTEGER,
-  //     FOREIGN KEY (category_id) REFERENCES category(id)
-  //   )
-  // `);
-
-  const insertCategory = async(name:string, target:number) => {
+  const insertCategory = async (name: string, target: number) => {
     const sql = `INSERT INTO category (name, target) VALUES (?, ?)`; // Adjust table name and columns as needed
 
     await db.run(sql, [name, target])
-};
-
-// await db.exec(`
-//   ALTER TABLE task ADD COLUMN reassign BOOLEAN NOT NULL DEFAULT FALSE;
-// `);
-
-// await db.exec(`
-//   ALTER TABLE habit ADD COLUMN weight REAL NOT NULL DEFAULT 0;
-// `);
-
-//const date = `DATE(DATETIME('now', 'localtime'))`
-// const date = `2024-03-20`
-
-// await db.exec(`UPDATE habit
-// SET date = '2025-03-27'  where id = 138 `)
-
-// const value = await db.exec(`SELECT 
-//     date AS task_day,
-//     category,
-//     SUM(weight) AS total_weight
-// FROM 
-//     task
-// WHERE 
-//     task_date >= CURDATE() AND 
-//     task_date < CURDATE() + INTERVAL 30 DAY
-// GROUP BY 
-//     task_day, category
-// ORDER BY 
-//     task_day, category`)
-
-// await db.exec(`UPDATE task
-// SET category_id = 1 where id < 46`);
-
-// await db.exec(`UPDATE habit
-// SET date = '2025-04-01' where date = '2025-04-02' `)
-// await db.exec(`DELETE FROM task `)
-// await db.exec(`DELETE FROM task where id >= 722 `)
-// const DATABASE_CREATE_REMINDERS = `
-//     CREATE TABLE IF NOT EXISTS reminders (
-//         id INTEGER PRIMARY KEY AUTOINCREMENT,
-//         task TEXT NOT NULL,
-//         date TEXT NOT NULL,
-//         time TEXT,                    -- Optional field
-//         repeat TEXT,
-//         completed BOOLEAN NOT NULL DEFAULT 0
-//     );
-// `;
-
-// await db.exec(DATABASE_CREATE_REMINDERS)
-await db.exec(`ALTER TABLE reminders 
-ADD COLUMN alloted INTEGER DEFAULT 30;`);
+  };
 
 
-console.log('done')
+  if (deleteTables) {
 
-// await db.exec(`
-//   CREATE TABLE habit (
-//     id INTEGER PRIMARY KEY AUTOINCREMENT,
-//     taskName TEXT NOT NULL,
-//     done BOOLEAN DEFAULT 0,
-//     procrastinated BOOLEAN DEFAULT 0,
-//     date TEXT NOT NULL
-//   )
-// `);
+    await db.exec(`DELETE FROM task `)
+    await db.exec(`DELETE FROM reminders `)
+    await db.exec(`DELETE FROM habit `)
+  }
 
 
 
-// Example usage
+  if (createTables){
+      // Create the category table
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS category (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      target INTEGER 
+    )
+  `);
+
+
+    // // Create the task table
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS task (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      description TEXT,
+      start_time TEXT,
+      end_time TEXT,
+      weight INTEGER,
+      completed BOOLEAN NOT NULL DEFAULT 0,
+      category_id INTEGER,
+      FOREIGN KEY (category_id) REFERENCES category(id)
+    )
+  `);
+
+   await db.exec(`
+    ALTER TABLE task ADD COLUMN reassign BOOLEAN NOT NULL DEFAULT FALSE;
+  `);
+
+ 
+
+
+    const DATABASE_CREATE_REMINDERS = `
+      CREATE TABLE IF NOT EXISTS reminders (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          task TEXT NOT NULL,
+          date TEXT NOT NULL,
+          time TEXT,                    -- Optional field
+          repeat TEXT,
+          completed BOOLEAN NOT NULL DEFAULT 0
+      );
+  `;
+
+  await db.exec(DATABASE_CREATE_REMINDERS)
+
+  await db.exec(`ALTER TABLE reminders 
+    ADD COLUMN alloted INTEGER DEFAULT 30;`);
+
+  
+      await db.exec(`
+    CREATE TABLE habit (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      taskName TEXT NOT NULL,
+      done BOOLEAN DEFAULT 0,
+      procrastinated BOOLEAN DEFAULT 0,
+      date TEXT NOT NULL
+    )
+  `);
+
+  await db.exec(`
+    ALTER TABLE habit ADD COLUMN weight REAL NOT NULL DEFAULT 0;
+  `);
+
+  insertCategory('General', 1);
+
+
+  }
+
+
+
+
+
+
+ 
+ 
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+  // Example usage
   // insertCategory('Health', 4);
   //health - morning exercise 1, meditation 1 ,evening exercise 2
 
